d6a307b9e0b33ece6f0180415ac94f78413906e9
Lagyu <sasaki.y@ruri.waseda.jp>
2017-11-20 11:37:18 +0900

    変数名の変更

--- ptcgcl/Move.py
+++ ptcgcl/Move.py
@@ -18,12 +18,14 @@ def draw():
         Board.BOARD_ELEM[Board.BOARD_DIC.index("DECK_0")].pop(0)
         return 0
 
+
 def shuffle_hand_into_deck():
     for i in range(len(Board.BOARD_ELEM[Board.BOARD_DIC.index("HAND_0")])):
         Board.BOARD_ELEM[Board.BOARD_DIC.index("DECK_0")].append(Board.BOARD_ELEM[Board.BOARD_DIC.index("HAND_0")][i])
     Board.BOARD_ELEM[Board.BOARD_DIC.index("HAND_0")] = []
     random.shuffle(Board.BOARD_ELEM[Board.BOARD_DIC.index("DECK_0")])
     return 0
+
 
 def mulligan():
     shuffle_hand_into_deck()
@@ -80,6 +82,7 @@ def attach_energy_from_hand(card_no: int, bench_id: int):
         return True
     else:
         return False
+
 
 def do_move(move_no: int):
     if Check.check_moveusable(move_no):
