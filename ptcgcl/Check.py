652a1a5bb82a18ecc6efb8702c4b58db8248da4a
Lagyu <sasaki.y@ruri.waseda.jp>
2017-11-21 15:56:23 +0900

    ワザ・特性の実装の準備

--- ptcgcl/Check.py
+++ ptcgcl/Check.py
@@ -1,6 +1,5 @@
 import json
 import re
-
 from . import Board
 
 '''
---
+++ ptcgcl/Translate_dict.py
@@ -0,0 +1,23 @@
+from . import Import_cards
+from . import Moves
+from . import Abilities
+
+moves_dict = {
+    "xy6-77" : [Moves.sky_return()],
+    "xy6-106" : [Moves.sky_return()],
+    
+
+
+
+
+}
+
+
+ability_dict = {
+    "xy6-77": [Abilities.sky_return()],
+    "xy6-106": [Abilities.sky_return()],
+
+
+
+
+}
